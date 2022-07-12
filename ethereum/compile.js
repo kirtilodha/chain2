305480d1de62c35e2c39d03a406293001e017cd4
kirtilodha <kirtilodha0@gmail.com>
2022-07-12 17:30:03 +0530

    initial commits

---
+++ ethereum/compile.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const solc = require('solc')
+const fs = require('fs-extra')
+
+// __dirname is equal to the current directory name, which is ethereum
+const buildPath = path.resolve(__dirname, 'build')
+// removes the file and all of its content at buildPath
+fs.removeSync(buildPath)
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Chain.sol')
+const source = fs.readFileSync(campaignPath, 'utf8')
+// output contains all the compiled contract code
+const output = solc.compile(source, 1).contracts
+
+// creates a new directory at buildPath
+fs.ensureDirSync(buildPath)
+
+for (const contract in output) {
+  // contract is the property name of the output object
+  // creates a file
+  // contract.replace(':', '') removes ':' that comes before the contract name
+  fs.outputJSONSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract],
+  )
+}
---
+++ ethereum/contracts/Chain.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.17;
+contract MainChain{
+    address[] public deployedChain;
+    function createChain(uint price) public{
+        address newChain = address(new Chain(price,msg.sender));
+        deployedChain.push(newChain);
+    }
+    function getDeployedChains() public view returns(address[] memory) {
+        return deployedChain;
+    }
+}
+contract Chain{
+    address public manager;
+    uint public costOfProduct;
+    mapping(address=>bool) public buyers;
+
+    modifier restricted(){
+        require(msg.sender==manager);
+        _;
+    }
+    constructor(uint price,address creator) public{
+        manager=creator;
+        costOfProduct = price;
+    }
+    function buy() public payable {
+        require(msg.value >= costOfProduct);
+        buyers[msg.sender]=true;
+    }
+}
